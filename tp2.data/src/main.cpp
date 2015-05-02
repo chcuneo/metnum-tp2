d84ecfea14aaa6650f8042a67f3241643660de0b
Chris <chriscuneo93@gmail.com>
2015-05-02 12:38:46 -0300

    Argumentos y carga de input

--- tp2.data/src/main.cpp
+++ tp2.data/src/main.cpp
@@ -3,17 +3,44 @@
 #include <stdlib.h>
 #include <iostream>
 #include <fstream>
+#include <vector>
+#include <string>
+
+using namespace std;
 
 int main(int argc, char *argv[]){
-	Matrix c(10, 5);
-	c(1, 1) = 1;
-	c(9, 1) = 2;
-	c(5, 4) = 3;
-	c(9, 4) = 4;
-	showMatCons(c);
-	c.transpuesta();
-	showMatCons(c);
-	system("pause");
+	//Validar argumentos
+	if (argc != 3) {
+		cout << "usage: ./tp <input file> <output file>" << endl;
+		return 0;
+	}
+	
+	//Cargar datos input
+	std::ifstream input;
+	input.open(argv[1]);
+
+	string databasename;
+	int k;
+	int alpha;
+	int Kfoldings;
+	vector<vector<bool>> crossval;
+
+	input >> databasename;
+	input >> k;
+	input >> alpha;
+	input >> Kfoldings;
+	
+	crossval.resize(Kfoldings);
+	int aux;
+	for (int line = 0; line < Kfoldings; line++) {
+		crossval[line].resize(42000);
+		for (int i = 0; i < 42000; i++) {
+			input >> aux;
+			crossval[line][i] = (bool)aux;
+		}
+	}
+
+
 	return 0;
 }
 
