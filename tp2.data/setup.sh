44147ac7bd8211851fd580189289d9f4327ebd96
christian cuneo <ccuneo@ws10.labo7>
2015-07-07 02:41:22 -0300

    Setup

--- tp2.data/setup.sh
+++ tp2.data/setup.sh
@@ -3,5 +3,6 @@ mkdir /tmp/ccuneoMN3/
 mkdir /tmp/ccuneoMN3/bin/
 mkdir /tmp/ccuneoMN3/inputs/
 mkdir /tmp/ccuneoMN3/data/
-cp -rf ./src/tests/ /tmp/ccuneoMN3/inputs/
-wget -x --load-cookies -P /tmp/ccuneoMN3/data/ cookies.txt https://www.kaggle.com/c/digit-recognizer/download/train.csv
+mkdir ./bin/
+cp -rf ./src/tests/ /tmp/ccuneoMN3/
+wget -x --load-cookies cookies.txt -P /tmp/ccuneoMN3/data/ https://www.kaggle.com/c/digit-recognizer/download/train.csv
